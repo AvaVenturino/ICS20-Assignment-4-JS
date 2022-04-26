5da0a6285026d94e2f70514b9a9ee7195a5ef48e
AvaVenturino <AvaVenturino@users.noreply.github.com>
2022-04-26 22:12:30 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -14,63 +14,51 @@ if (navigator.serviceWorker) {
 }
 
 function getPizza() {
-    var price = 0;
-    var size = "";
-    var top = 0;
-    var total = 0;
-            
-    var s1 = document.getElementById("s1");
-    var s2 = document.getElementById("s2");
-    
-    if(s1.checked==true)
-    {
-    price = 6.00;
+  var price = 0;
+  var size = "";
+  var top = 0;
+  var total = 0;
+
+  var s1 = document.getElementById("s1");
+  var s2 = document.getElementById("s2");
+
+  if (s1.checked == true) {
+    price = 6.0;
     size = "large";
-    }   
-    else if(s2.checked==true)
-    {
-    price = 10.00;
+  } else if (s2.checked == true) {
+    price = 10.0;
     size = "extraLarge";
-    }
+  }
 
-var t1 = document.getElementById("topping1");
-var t2 = document.getElementById("topping2");
-var t3 = document.getElementById("topping3");
-var t4 = document.getElementById("topping4");
+  var t1 = document.getElementById("topping1");
+  var t2 = document.getElementById("topping2");
+  var t3 = document.getElementById("topping3");
+  var t4 = document.getElementById("topping4");
 
-if(t1 == true) {
-top = top + 1.0;
-document.getElementById("t_options").innerHTML = "1topping";
-}
-else
-top = top;
+  if (t1 == true) {
+    top = top + 1.0;
+    document.getElementById("t_options").innerHTML = "1topping";
+  } else top = top;
 
-if(t2 == true) {
-top = top + 1.75;
-document.getElementById("t_options").innerHTML = "2topping";
-}
-else 
-top = top;
+  if (t2 == true) {
+    top = top + 1.75;
+    document.getElementById("t_options").innerHTML = "2topping";
+  } else top = top;
 
-if(t3 == true) {
-top = top + 2.50;
-  document.getElementById("t_options").innerHTML = "3topping";
-}
-else 
-top = top;
+  if (t3 == true) {
+    top = top + 2.5;
+    document.getElementById("t_options").innerHTML = "3topping";
+  } else top = top;
 
-if(t4 == true) {
-top = top + 3.35;
-  document.getElementById("t_options").innerHTML = "4topping";
-}
-else 
-top = top;
+  if (t4 == true) {
+    top = top + 3.35;
+    document.getElementById("t_options").innerHTML = "4topping";
+  } else top = top;
 
   document.getElementById("t_result").innerHTML = "$ " + top;
-  
-  const total = (size + toppings) * 0.13
-  
-        document.getElementById("total_result").innerHTML = 
-        "Your Current Total is $ " + total;
 
+  const total = (size + toppings) * 0.13;
+
+  document.getElementById("total_result").innerHTML =
+    "Your Current Total is $ " + total;
 }

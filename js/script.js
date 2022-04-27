c0440a9372c9d45bf388bed52a04f689b8c2934b
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-27 17:10:53 +0000

    edited js

--- js/script.js
+++ js/script.js
@@ -14,51 +14,56 @@ if (navigator.serviceWorker) {
 }
 // Input
 function getPizza() {
-  var price = 0;
-  var size = "";
-  var top = 0;
-  var total = 0;
+  var price = 0
+  var size = ""
+  var top = 0
+  var total = 0
+  var tax = 0
 
-  var s1 = document.getElementById("s1");
-  var s2 = document.getElementById("s2");
+  var s1 = document.getElementById("s1")
+  var s2 = document.getElementById("s2")
 
   if (s1.checked == true) {
-    price = 6.0;
-    size = "large";
+    price = 6.0
+    size = "large"
   } else if (s2.checked == true) {
-    price = 10.0;
-    size = "extraLarge";
+    price = 10.0
+    size = "extraLarge"
   }
 
-  var t1 = document.getElementById("topping1");
-  var t2 = document.getElementById("topping2");
-  var t3 = document.getElementById("topping3");
-  var t4 = document.getElementById("topping4");
+  var t1 = document.getElementById("topping1")
+  var t2 = document.getElementById("topping2")
+  var t3 = document.getElementById("topping3")
+  console.log(t3)
+  var t4 = document.getElementById("topping4")
 
-  if (t1 == true) {
-    top = top + 1.0;
+  if (t1.checked == true) {
+    top = 1.0
     document.getElementById("t_options").innerHTML = "1topping";
-  } else top = top;
+  }
 
-  if (t2 == true) {
-    top = top + 1.75;
+  if (t2.checked == true) {
+    top = 1.75
     document.getElementById("t_options").innerHTML = "2topping";
-  } else top = top;
+  }
 
-  if (t3 == true) {
-    top = top + 2.5;
-    document.getElementById("t_options").innerHTML = "3topping";
-  } else top = top;
+  if (t3.checked == true) {
+    top = 2.5
+    document.getElementById("t_options").innerHTML = "3topping"
+  } 
 
-  if (t4 == true) {
-    top = top + 3.35;
-    document.getElementById("t_options").innerHTML = "4topping";
-  } else top = top;
+  if (t4.checked == true) {
+    top = 3.35
+    document.getElementById("t_options").innerHTML = "4topping"
+  }
 
-  document.getElementById("t_result").innerHTML = "$ " + top;
+  document.getElementById("t_result").innerHTML = "$ " + top
 
-  total = (size + toppings) * 0.13;
+  console.log(price)
+  console.log(top)
+  tax = (price + top) * 0.13
+  total = tax + (price + top)
 
   document.getElementById("total_result").innerHTML =
-    "Your Current Total is $ " + total;
+    "Your Current Total is $ " + total
 }

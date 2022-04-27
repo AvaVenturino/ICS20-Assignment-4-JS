6e97d87480fc72db6e88825aa3529bf44684045b
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-27 21:00:57 +0000

    deleted commas

--- js/script.js
+++ js/script.js
@@ -22,18 +22,18 @@ if (navigator.serviceWorker) {
   var total = 0
   var tax = 0
 
-  var s1 = document.getElementById("s1");
-  var s2 = document.getElementById("s2");
+  var s1 = document.getElementById("s1")
+  var s2 = document.getElementById("s2")
 
   if (s1.checked == true) {
-    price = 6.0;
-    document.getElementById("s_result").innerHTML = "Large";
+    price = 6.0
+    document.getElementById("s_result").innerHTML = "Large"
   } else if (s2.checked == true) {
-    price = 10.0;
-    document.getElementById("s_result").innerHTML = "Extra Large";
+    price = 10.0
+    document.getElementById("s_result").innerHTML = "Extra Large"
   }
 
-  document.getElementById("p_result").innerHTML = "$ " + price;
+  document.getElementById("p_result").innerHTML = "$ " + price
 
   var t1 = document.getElementById("topping1");
   var t2 = document.getElementById("topping2");
@@ -41,30 +41,30 @@ if (navigator.serviceWorker) {
   var t4 = document.getElementById("topping4");
 
   if (t1.checked == true) {
-    top = 1.0;
-    document.getElementById("t_options").innerHTML = "1 Topping";
+    top = 1.0
+    document.getElementById("t_options").innerHTML = "1 Topping"
   }
 
   if (t2.checked == true) {
-    top = 1.75;
-    document.getElementById("t_options").innerHTML = "2 Toppings";
+    top = 1.75
+    document.getElementById("t_options").innerHTML = "2 Toppings"
   }
 
   if (t3.checked == true) {
-    top = 2.5;
-    document.getElementById("t_options").innerHTML = "3 Toppings";
+    top = 2.5
+    document.getElementById("t_options").innerHTML = "3 Toppings"
   }
 
   if (t4.checked == true) {
-    top = 3.35;
-    document.getElementById("t_options").innerHTML = "4 Toppings";
+    top = 3.35
+    document.getElementById("t_options").innerHTML = "4 Toppings"
   }
 
-  document.getElementById("t_result").innerHTML = "$ " + top;
+  document.getElementById("t_result").innerHTML = "$ " + top
 
-  tax = (price + top) * 0.13;
-  total = tax + (price + top);
+  tax = (price + top) * 0.13
+  total = tax + (price + top)
 
   document.getElementById("total_result").innerHTML =
-    "Your Current Total is $ " + total.toFixed(2);
+    "Your Current Total is $ " + total.toFixed(2)
 }
